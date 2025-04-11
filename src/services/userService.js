2fd5a4839db1c81849dfb8888b180e7d62771bf8
Teddy <ted.j.gallion@gmail.com>
2025-04-10 20:25:51 -0500

    WIP: user service hotfix

--- src/services/userService.js
+++ src/services/userService.js
@@ -22,7 +22,11 @@ const index = async () => {
 
  const getFavorites = async (userId) => {
     try {
-      const res = await fetch(`${BASE_URL}/${userId}/favorites`);
+      const res = await fetch(`${BASE_URL}/${userId}/favorites`,{
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('token')}`
+      }
+      });
       return res.json();
     } catch (error) {
       console.error("Error fetching favorite cocktail:", error);
